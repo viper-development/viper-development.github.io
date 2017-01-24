00aa56afde2abbfd69b75ecd40d1138f224b5f0f
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-01-24 12:46:26 +0100

    form: Use multiline string

--- assets/app.js
+++ assets/app.js
@@ -58,7 +58,8 @@ app.controller('Data', ['$http', function($http , $scope){
                 "Get a recurring ROI with every single session"
             ],
             "price": "800eur/mon",
-            "form": "<a class="button" href="https://viperdev.typeform.com/to/fABj8j" data-mode="1" target="_blank">Get Started</a><script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}id=id+'_';if(!gi.call(d,id)){qs=ce.call(d,'link');qs.rel='stylesheet';qs.id=id;qs.href=b+'share-button.css';s=gt.call(d,'head')[0];s.appendChild(qs,s)}})()</script>"
+            "form": `<a class="button" href="https://viperdev.typeform.com/to/fABj8j" data-mode="1" target="_blank">Get Started</a>
+            <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}id=id+'_';if(!gi.call(d,id)){qs=ce.call(d,'link');qs.rel='stylesheet';qs.id=id;qs.href=b+'share-button.css';s=gt.call(d,'head')[0];s.appendChild(qs,s)}})()</script>"``
         },
         {
             "title" : "Boost your Open Source Project",
