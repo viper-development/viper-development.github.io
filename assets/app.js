e769a555937fc4d0a594fcf074762e6bbe447a80
Hemang Kumar <hemangsk@gmail.com>
2017-02-13 20:53:08 +0530

    app.js: Add Team directive
    This moves team members json object
    inside Team directive.

--- assets/app.js
+++ assets/app.js
@@ -24,52 +24,62 @@ app.controller('TabController',['$location','$scope', function ($location, $scop
         return $location.path() == stab
     }
 }])
+app.directive('team', function () {
+    return{
+        restrict: 'E',
+        templateUrl: '/partials/tabs/team.html',
+        controller: function () {
+            self = this;
+            self.profile = [
+                {
+                    "name" : "Lasse Schuirmann",
+                    "id" : "sils",
+                    "picture" : "https://avatars2.githubusercontent.com/u/5716520",
+                    "short_tagline": "Problem solver",
+                    "bio": "Founder of <a href='http://coala.io'>coala</a>, <a href='http://gitmate.io'>GitMate</a> and viperdev and conference speaker."
+                },
+                {
+                    "name" : "Mischa Krüger",
+                    "id" : "Makman2",
+                    "picture" : "https://avatars3.githubusercontent.com/u/6023916",
+                    "short_tagline": "Code & Review Junkie",
+                    "bio": "<a href='http://coala.io'>coala</a> core developer and inventor of the open source CE3D terminal game engine."
+                },
+                {
+                    "name" : "Adhityaa Chandrasekar",
+                    "id" : "adtac",
+                    "picture" : "https://avatars2.githubusercontent.com/u/7521600",
+                    "short_tagline": "Hardcore hacker",
+                    "bio": "<a href='http://coala.io'>coala</a> core developer and creator of climate and fssb."
+                },
+                {
+                    "name" : "Maximilian Scholz",
+                    "id" : "sims1253",
+                    "picture" : "https://avatars3.githubusercontent.com/u/6530123",
+                    "short_tagline": "Community evangelist",
+                    "bio": "Leader of the <a href='http://coala.io'>coala</a> community team and creator of the LineSpots algorithm."
+                },
+                {
+                    "name" : "Sebastian Latacz",
+                    "id" : "seblat",
+                    "picture": "https://avatars0.githubusercontent.com/u/18051901",
+                    "short_tagline":"Business expert",
+                    "bio" : "Founder of <a href='http://gitmate.io'>GitMate</a> and solver of the most complicated business problems."
+                },
+                {
+                    "name" : "Hemang Kumar",
+                    "id" : "hemangsk",
+                    "picture": "https://avatars1.githubusercontent.com/u/13018570",
+                    "short_tagline":"Frontend Enthusiast",
+                    "bio" : "<a href='http://coala.io'>coala</a> developer. Frontend lover. Creates and designs web frontends with passion."
+                }
+            ]
+        },
+        controllerAs: "dc"
+    }
+})
 app.controller('Data', ['$http', function($http , $scope){
     self = this;
-    self.profile = [
-        {
-            "name" : "Lasse Schuirmann",
-            "id" : "sils",
-            "picture" : "https://avatars2.githubusercontent.com/u/5716520",
-            "short_tagline": "Problem solver",
-            "bio": "Founder of <a href='http://coala.io'>coala</a>, <a href='http://gitmate.io'>GitMate</a> and viperdev and conference speaker."
-        },
-        {
-            "name" : "Mischa Krüger",
-            "id" : "Makman2",
-            "picture" : "https://avatars3.githubusercontent.com/u/6023916",
-            "short_tagline": "Code & Review Junkie",
-            "bio": "<a href='http://coala.io'>coala</a> core developer and inventor of the open source CE3D terminal game engine."
-        },
-        {
-            "name" : "Adhityaa Chandrasekar",
-            "id" : "adtac",
-            "picture" : "https://avatars2.githubusercontent.com/u/7521600",
-            "short_tagline": "Hardcore hacker",
-            "bio": "<a href='http://coala.io'>coala</a> core developer and creator of climate and fssb."
-        },
-        {
-            "name" : "Maximilian Scholz",
-            "id" : "sims1253",
-            "picture" : "https://avatars3.githubusercontent.com/u/6530123",
-            "short_tagline": "Community evangelist",
-            "bio": "Leader of the <a href='http://coala.io'>coala</a> community team and creator of the LineSpots algorithm."
-        },
-        {
-            "name" : "Sebastian Latacz",
-            "id" : "seblat",
-            "picture": "https://avatars0.githubusercontent.com/u/18051901",
-            "short_tagline":"Business expert",
-            "bio" : "Founder of <a href='http://gitmate.io'>GitMate</a> and solver of the most complicated business problems."
-        },
-        {
-            "name" : "Hemang Kumar",
-            "id" : "hemangsk",
-            "picture": "https://avatars1.githubusercontent.com/u/13018570",
-            "short_tagline":"Frontend Enthusiast",
-            "bio" : "<a href='http://coala.io'>coala</a> developer. Frontend lover. Creates and designs web frontends with passion."
-        }
-    ]
 
     self.plans = [
         {
